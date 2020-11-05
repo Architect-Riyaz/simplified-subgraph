afb90ee58aa819d210aaabcec50516f67a8fafe6
riyazuddin_shaikh <36470249+Architect-Riyaz@users.noreply.github.com>
2020-11-05 13:16:45 +0530

    Initial Changes

--- src/mappings/dayUpdates.ts
+++ src/mappings/dayUpdates.ts
@@ -1,31 +1,31 @@
 import { PairHourData } from './../types/schema'
 /* eslint-disable prefer-const */
 import { BigInt, BigDecimal, EthereumEvent } from '@graphprotocol/graph-ts'
-import { Pair, Bundle, Token, UniswapFactory, UniswapDayData, PairDayData, TokenDayData } from '../types/schema'
+import { Pair, Bundle, Token, SimplifiedFactory, SimplifiedDayData, PairDayData, TokenDayData } from '../types/schema'
 import { ONE_BI, ZERO_BD, ZERO_BI, FACTORY_ADDRESS } from './helpers'
 
-export function updateUniswapDayData(event: EthereumEvent): UniswapDayData {
-  let uniswap = UniswapFactory.load(FACTORY_ADDRESS)
+export function updateSimplifiedDayData(event: EthereumEvent): SimplifiedDayData {
+  let Simplified = SimplifiedFactory.load(FACTORY_ADDRESS)
   let timestamp = event.block.timestamp.toI32()
   let dayID = timestamp / 86400
   let dayStartTimestamp = dayID * 86400
-  let uniswapDayData = UniswapDayData.load(dayID.toString())
-  if (uniswapDayData === null) {
-    uniswapDayData = new UniswapDayData(dayID.toString())
-    uniswapDayData.date = dayStartTimestamp
-    uniswapDayData.dailyVolumeUSD = ZERO_BD
-    uniswapDayData.dailyVolumeETH = ZERO_BD
-    uniswapDayData.totalVolumeUSD = ZERO_BD
-    uniswapDayData.totalVolumeETH = ZERO_BD
-    uniswapDayData.dailyVolumeUntracked = ZERO_BD
+  let SimplifiedDayData = SimplifiedDayData.load(dayID.toString())
+  if (SimplifiedDayData === null) {
+    SimplifiedDayData = new SimplifiedDayData(dayID.toString())
+    SimplifiedDayData.date = dayStartTimestamp
+    SimplifiedDayData.dailyVolumeUSD = ZERO_BD
+    SimplifiedDayData.dailyVolumeETH = ZERO_BD
+    SimplifiedDayData.totalVolumeUSD = ZERO_BD
+    SimplifiedDayData.totalVolumeETH = ZERO_BD
+    SimplifiedDayData.dailyVolumeUntracked = ZERO_BD
   }
 
-  uniswapDayData.totalLiquidityUSD = uniswap.totalLiquidityUSD
-  uniswapDayData.totalLiquidityETH = uniswap.totalLiquidityETH
-  uniswapDayData.txCount = uniswap.txCount
-  uniswapDayData.save()
+  SimplifiedDayData.totalLiquidityUSD = Simplified.totalLiquidityUSD
+  SimplifiedDayData.totalLiquidityETH = Simplified.totalLiquidityETH
+  SimplifiedDayData.txCount = Simplified.txCount
+  SimplifiedDayData.save()
 
-  return uniswapDayData as UniswapDayData
+  return SimplifiedDayData as SimplifiedDayData
 }
 
 export function updatePairDayData(event: EthereumEvent): PairDayData {
